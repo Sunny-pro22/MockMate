74e2c078d7cb3ea29810202fb03e923de1be4c8e
Sunny <gurujii4545@gmail.com>
2025-06-26 01:49:35 +0530

    Add Vite as dev dependency

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
   plugins: [react()],
   base: './',
 });
