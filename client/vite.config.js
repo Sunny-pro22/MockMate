2ea675aae5ec2012ca1c9dca4c41e522d6c811ce
Sunny <gurujii4545@gmail.com>
2025-06-26 02:00:09 +0530

    Production setup for Render deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+   
+    allowedHosts: ['mockmate-5.onrender.com'], 
+
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
   plugins: [react()],
   base: './',
-  server: {
-    allowedHosts: ['mockmate-5.onrender.com'], // ✅ Allow Render host
-  },
 });
