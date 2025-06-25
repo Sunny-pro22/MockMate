6c0ffc49fec6c28041359d4afe73c0d5e67a5c42
Sunny <gurujii4545@gmail.com>
2025-06-26 01:52:58 +0530

    Add Vite as dev dependency

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-  },
   plugins: [react()],
   base: './',
+  server: {
+    allowedHosts: ['mockmate-5.onrender.com'], // ✅ Allow Render host
+  },
 });
